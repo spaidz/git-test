07bd078a5b5cd861c45a0c8d464bdf62dd42d927
Kanishka Gaveen Ratnajothy <kgaveen@gmail.com>
2021-03-04 11:37:46 +0530

    Third change

--- Readme.txt
+++ Readme.txt
@@ -1,2 +1,3 @@
 This contains a hello world print statement
 blah blah blah
+my name is gaveen
--- new.py
+++ new.py
@@ -1,6 +1,4 @@
-Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print("Hello World!!")
+print("Hello World!!")
 
 
 
